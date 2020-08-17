a27c282551d7c96e909d51f12930b8f9a91a03f9
Vinícius Cantuária <cantuariavc@gmail.com>
2020-08-17 15:50:40 -0300

    I Am Rich 1.0

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Vinícius Cantuária on 17/08/20.
+//  Copyright © 2020 Vinícius Cantuária. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
